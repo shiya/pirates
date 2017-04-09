4f782b4ede6769dde5ee3122e1932aea7f3643b1
shiya <luo.shiya@gmail.com>
2017-04-09 14:41:49 -0700

    destroy projectile after a while

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 110f);
+	}
+
+}
